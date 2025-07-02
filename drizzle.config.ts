d8e002db8a35fac952aac98305ccfc1f224a98bf
Vinícios Costa <dev.vinicios@gmail.com>
2025-07-02 14:16:27 -0300

    refactor: change folder name

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { env } from '@/shared/config/env'
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-  out: './src/infra/db/drizzle',
-  schema: './src/infra/db/schemas.ts',
+  out: './src/infra/data/drizzle',
+  schema: './src/infra/data/schemas.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: env.DATABASE_URL
