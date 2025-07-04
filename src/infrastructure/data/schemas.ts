7c42a9189e6bf256daa22c5538e088f9a46e327d
Vinícios Costa <dev.vinicios@gmail.com>
2025-07-04 15:24:32 -0300

    raw: change tables names for better legibility

--- src/infrastructure/data/schemas.ts
+++ src/infrastructure/data/schemas.ts
@@ -1,19 +1,19 @@
 import { pgTable, uuid, varchar, date, boolean } from 'drizzle-orm/pg-core'
 
-export const users = pgTable('users', {
+export const usersTable = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
   name: varchar('name', { length: 60 }).notNull(),
   email: varchar('email', { length: 120 }).notNull().unique(),
   password: varchar('password', { length: 60 }).notNull()
 })
 
-export const tasks = pgTable('tasks', {
+export const tasksTable = pgTable('tasks', {
   id: uuid('id').primaryKey().defaultRandom(),
   title: varchar('title', { length: 60 }).notNull(),
   description: varchar('description', { length: 255 }),
   dueDate: date('due_date', { mode: 'date' }).notNull(),
   isCompleted: boolean('is_completed').notNull().default(false),
   userId: uuid('user_id')
-    .references(() => users.id, { onDelete: 'cascade' })
+    .references(() => usersTable.id, { onDelete: 'cascade' })
     .notNull()
 })
