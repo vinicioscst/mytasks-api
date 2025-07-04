d9389bc7b76e4e14556e9b1571416f6384f5e95b
Vinícios Costa <dev.vinicios@gmail.com>
2025-07-04 15:23:27 -0300

    fix: comment unused routes to avoid TypeError

--- src/presentation/routes/tasksRoutes.ts
+++ src/presentation/routes/tasksRoutes.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express'
 
-const tasksRoutes: Router = Router()
+export const tasksRoutes: Router = Router()
 
-tasksRoutes.get('')
-tasksRoutes.post('')
-tasksRoutes.get('/:id')
-tasksRoutes.put('/:id')
-tasksRoutes.delete('/:id')
-
-export { tasksRoutes }
+// tasksRoutes.get('')
+// tasksRoutes.post('')
+// tasksRoutes.get('/:id')
+// tasksRoutes.put('/:id')
+// tasksRoutes.delete('/:id')
--- src/presentation/routes/usersRoutes.ts
+++ src/presentation/routes/usersRoutes.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express'
 
-const usersRoutes: Router = Router()
+export const usersRoutes: Router = Router()
 
-usersRoutes.post('')
-usersRoutes.post('/login')
-usersRoutes.get('/:id')
-usersRoutes.patch('/:id')
-usersRoutes.delete('/:id')
-
-export { usersRoutes }
+// usersRoutes.post('')
+// usersRoutes.get('/:id')
+// usersRoutes.patch('/:id')
+// usersRoutes.delete('/:id')
