f2abd00a060be5703ac8cf5801325e3a9be21d12
Vinícios Costa <dev.vinicios@gmail.com>
2025-07-08 13:23:35 -0300

    feat: create read user logic

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { UserApplicationService } from '@/application/services/UserApplicationService'
+import { NotFoundError } from '@/shared/helpers/ApiErrors'
 
 export class UserController {
   userApplicationService: UserApplicationService
@@ -13,5 +14,12 @@ export class UserController {
     const result = await this.userApplicationService.createUser(body)
 
     res.status(201).json(result)
+  }
+
+  async readUser(_req: Request, res: Response) {
+    const { user } = res.locals
+    if (!user) throw new NotFoundError('Usuário não encontrado')
+
+    res.status(200).json(user)
   }
 }
--- src/presentation/routes/usersRoutes.ts
+++ src/presentation/routes/usersRoutes.ts
@@ -1,12 +1,14 @@
 import { Router, Request, Response } from 'express'
 import { UserController } from '../controllers/UserController'
+import { authMiddleware } from '../middlewares/authMiddleware'
+import { permissionMiddleware } from '../middlewares/permissionMiddleware'
 
 export const usersRoutes: Router = Router()
 const userController = new UserController()
 
-usersRoutes.post('', (req: Request, res: Response) =>
-  userController.createUser(req, res)
-)
-// usersRoutes.get('/:id')
+usersRoutes.use('/:id', authMiddleware, permissionMiddleware)
+
+usersRoutes.post('', userController.createUser)
+usersRoutes.get('/:id', userController.readUser)
 // usersRoutes.patch('/:id')
 // usersRoutes.delete('/:id')
