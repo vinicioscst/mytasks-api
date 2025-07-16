af152a98b20b26820ecff9d029c960c0df60a9ca
Vinícios Costa <dev.vinicios@gmail.com>
2025-07-15 23:48:55 -0300

    feat: add cookies parser and cors config

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,21 @@ import { usersRoutes } from './presentation/routes/usersRoutes'
 import { tasksRoutes } from './presentation/routes/tasksRoutes'
 import { authRoute } from './presentation/routes/authRoute'
 import { errorHandlerMiddleware } from './presentation/middlewares/errorHandlerMiddleware'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 export const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  })
+)
 
 app.use('/api/users', usersRoutes)
 app.use('/api/auth', authRoute)
