1e3637bca877d997e6c87ef5fb4fa33c3bdf2bef
Vinícios Costa <dev.vinicios@gmail.com>
2025-07-15 23:54:31 -0300

    feat: create logout logic and adjust read user

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { UserApplicationService } from '@/application/services/UserApplicationService'
 import { BadRequestError, NotFoundError } from '@/shared/helpers/ApiErrors'
+import { createHash } from 'node:crypto'
 
 export class UserController {
   userApplicationService: UserApplicationService
@@ -20,7 +21,25 @@ export class UserController {
     const { user } = res.locals
     if (!user) throw new NotFoundError('Usuário não encontrado')
 
-    res.status(200).json(user)
+    const hash = createHash('sha256')
+    const avatar = hash.update(user.email.toLowerCase()).digest('hex')
+
+    res
+      .status(200)
+      .json({ ...user, avatar: `https://gravatar.com/avatar/${avatar}` })
+  }
+
+  async logoutUser(_req: Request, res: Response) {
+    const { user } = res.locals
+    if (!user) throw new NotFoundError('Usuário não encontrado')
+
+    res
+      .status(204)
+      .cookie('token', '', {
+        httpOnly: true,
+        expires: new Date(0)
+      })
+      .end()
   }
 
   async updateUser(req: Request, res: Response) {
--- src/presentation/routes/usersRoutes.ts
+++ src/presentation/routes/usersRoutes.ts
@@ -1,18 +1,21 @@
 import { Router, Request, Response } from 'express'
 import { UserController } from '../controllers/UserController'
 import { authMiddleware } from '../middlewares/authMiddleware'
-import { permissionMiddleware } from '../middlewares/permissionMiddleware'
 
 export const usersRoutes: Router = Router()
 const userController = new UserController()
 
-usersRoutes.use('/:id', authMiddleware, permissionMiddleware)
-
 usersRoutes.post('', (req: Request, res: Response) =>
   userController.createUser(req, res)
 )
-usersRoutes.get('/:id', (req: Request, res: Response) =>
+
+usersRoutes.use('', authMiddleware)
+
+usersRoutes.get('/profile', (req: Request, res: Response) =>
   userController.readUser(req, res)
+)
+usersRoutes.post('/logout', (req: Request, res: Response) =>
+  userController.logoutUser(req, res)
 )
 usersRoutes.patch('/:id', (req: Request, res: Response) =>
   userController.updateUser(req, res)
