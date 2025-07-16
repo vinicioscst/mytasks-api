630677359a808acad20d0592c61a214ce9bffd70
Vinícios Costa <dev.vinicios@gmail.com>
2025-07-15 23:53:34 -0300

    feat: add logic to send token inside the cookies and user avatar

--- src/presentation/controllers/AuthController.ts
+++ src/presentation/controllers/AuthController.ts
@@ -1,5 +1,9 @@
 import { AuthApplicationService } from '@/application/services/AuthApplicationService'
+import { env } from '@/shared/config/env'
+import { TokenPayload } from '@/shared/types/token-payload'
 import { Request, Response } from 'express'
+import { verify } from 'jsonwebtoken'
+import { createHash } from 'node:crypto'
 
 export class AuthController {
   authApplicationService: AuthApplicationService
@@ -12,6 +16,17 @@ export class AuthController {
     const { body } = req
     const result = await this.authApplicationService.login(body)
 
-    res.status(200).json(result)
+    const { exp } = verify(result.token, env.JWT_SECRET) as TokenPayload
+
+    const hash = createHash('sha256')
+    const avatar = hash.update(result.user.email.toLowerCase()).digest('hex')
+
+    res
+      .status(200)
+      .cookie('token', result.token, {
+        httpOnly: true,
+        expires: new Date(exp * 1000)
+      })
+      .json({ ...result.user, avatar: `https://gravatar.com/avatar/${avatar}` })
   }
 }
