b7c82ffc2b74506d5588bf14a3cdff122a88d5d2
Vinícios Costa <dev.vinicios@gmail.com>
2025-07-15 23:52:56 -0300

    fix: adjust middleware to recognize cookies

--- src/presentation/middlewares/authMiddleware.ts
+++ src/presentation/middlewares/authMiddleware.ts
@@ -1,25 +1,25 @@
 import { DrizzleUserRepository } from '@/infrastructure/repositories/DrizzleUserRepository'
 import { env } from '@/shared/config/env'
 import { UnauthorizedError } from '@/shared/helpers/ApiErrors'
+import { TokenPayload } from '@/shared/types/token-payload'
 import { NextFunction, Request, Response } from 'express'
 import { verify } from 'jsonwebtoken'
-
-type TokenPayload = {
-  id: string
-}
 
 export async function authMiddleware(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> {
-  const { authorization } = req.headers
-  if (!authorization) throw new UnauthorizedError('Token ausente')
-
-  const token = authorization.split(' ')[1]
+  const token = req.cookies.token
   if (!token) throw new UnauthorizedError('Token ausente')
 
-  const { id } = verify(token, env.JWT_SECRET) as TokenPayload
+  const { id, exp } = verify(token, env.JWT_SECRET) as TokenPayload
+
+  const expirationTime = exp * 1000
+  const currentTime = Date.now()
+
+  if (currentTime >= expirationTime)
+    throw new UnauthorizedError('Token inválido')
 
   const user = await new DrizzleUserRepository().findById(id)
   if (!user) throw new UnauthorizedError('Token inválido')
