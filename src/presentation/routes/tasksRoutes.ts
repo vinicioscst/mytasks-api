17b216a6116390edd4428e1dd749e1be7f21e525
Vinícios Costa <dev.vinicios@gmail.com>
2025-07-21 16:03:53 -0300

    fix: adjust dependecy injection on controllers and middlewares order

--- src/presentation/routes/tasksRoutes.ts
+++ src/presentation/routes/tasksRoutes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import { authMiddleware } from '../middlewares/authMiddleware'
 import { permissionMiddleware } from '../middlewares/permissionMiddleware'
 import { TaskController } from '../controllers/TaskController'
@@ -6,10 +6,21 @@ import { TaskController } from '../controllers/TaskController'
 export const tasksRoutes: Router = Router()
 const taskController = new TaskController()
 
-tasksRoutes.use('', authMiddleware, permissionMiddleware)
+tasksRoutes.use('', authMiddleware)
+tasksRoutes.post('', (req: Request, res: Response) =>
+  taskController.createTask(req, res)
+)
+tasksRoutes.get('', (req: Request, res: Response) =>
+  taskController.readAllTasks(req, res)
+)
 
-tasksRoutes.post('', taskController.createTask)
-tasksRoutes.get('', taskController.readAllTasks)
-tasksRoutes.get('/:id', taskController.readTask)
-tasksRoutes.patch('/:id', taskController.updateTask)
-tasksRoutes.delete('/:id', taskController.deleteTask)
+tasksRoutes.use('/:id', permissionMiddleware)
+tasksRoutes.get('/:id', (req: Request, res: Response) =>
+  taskController.readTask(req, res)
+)
+tasksRoutes.patch('/:id', (req: Request, res: Response) =>
+  taskController.updateTask(req, res)
+)
+tasksRoutes.delete('/:id', (req: Request, res: Response) =>
+  taskController.deleteTask(req, res)
+)
