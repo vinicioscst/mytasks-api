eafcd751da7ff735e0d26ab7ec4099d17d17a14f
Vinícios Costa <dev.vinicios@gmail.com>
2025-08-25 10:10:17 -0300

    chore: add biome config

--- src/shared/helpers/ApiErrors.ts
+++ src/shared/helpers/ApiErrors.ts
@@ -1,5 +1,8 @@
 export class ApiError extends Error {
-  constructor(public message: string, public status: number) {
+  constructor(
+    public message: string,
+    public status: number
+  ) {
     super(message)
   }
 }
